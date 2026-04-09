34184e80a00b7d7f6372087d5c0926035110203f
Joel Partida <caburet@gmail.com>
2026-04-09 15:46:13 -0300

    arreglo

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/liga-del-backlog',
   images: {
     unoptimized: true,
     remotePatterns: [
